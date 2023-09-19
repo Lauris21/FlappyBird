6f2f2aaf85668cb7976d5b030104b10d4eb97244
Lauris21 <laurasanchez0301@gmail.com>
2023-09-19 10:10:31 +0200

    change webpack.prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,27 +1,29 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   devtool: false,
   performance: {
     maxEntrypointSize: 90000,
-    maxAssetSize: 900000
+    maxAssetSize: 900000,
   },
   optimization: {
     minimizer: [
       new TerserPlugin({
         terserOptions: {
           output: {
-            comments: false
-          }
-        }
-      })
-    ]
+            comments: false,
+            filename: "bundle.js",
+            path: path.resolve(__dirname, "build"),
+            clean: true,
+            assetModuleFilename: "[path][name][ext]",
+          },
+        },
+      }),
+    ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-  ]
-})
+  plugins: [new CleanWebpackPlugin()],
+});
